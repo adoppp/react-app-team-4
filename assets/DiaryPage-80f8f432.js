a74f5871f652e05ad03d96c648d7efc127c1d510
adoppp <121878757+adoppp@users.noreply.github.com>
2023-11-09 20:14:43 +0000

    Deploying to gh-pages from @ adoppp/react-app-team-4@46e3de908a78d14c9c6bf960b2737f453920573c 🚀

---
+++ assets/DiaryPage-80f8f432.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-eaea10fc.js";const a=()=>r.jsx("div",{children:r.jsx("h2",{children:"DiaryPage"})});export{a as default};
--- assets/ProfilePage-f87006df.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-880b1c15.js";const i=()=>e.jsx("div",{children:e.jsx("h2",{children:"ProfilePage"})});export{i as default};
