42b8e7f843d4cceb6c839a9949c0ce330ae3273b
viktoriia-2002 <92129770+viktoriia-2002@users.noreply.github.com>
2023-11-11 08:42:43 -0500

    DiaryPage Calendar
    Calendar with styles/ No icons

--- src/components/pages/DiaryPage/DiaryPage.jsx
+++ src/components/pages/DiaryPage/DiaryPage.jsx
@@ -10,3 +10,16 @@ const DiaryPage = () => {
 };
 
 export default DiaryPage;
+
+// import DatePickerCalendar from '../../ui/DatePicker/DatePicker';
+
+// const DiaryPage = () => {
+//   return (
+//     <div>
+//       <h2>DiaryPage</h2>
+//       <DatePickerCalendar />
+//     </div>
+//   );
+// };
+
+// export default DiaryPage;
