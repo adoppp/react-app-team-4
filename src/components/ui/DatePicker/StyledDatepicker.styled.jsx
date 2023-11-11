80dee06770b2e513db32f1ebe93ba61a4a057521
viktoriia-2002 <92129770+viktoriia-2002@users.noreply.github.com>
2023-11-10 22:01:30 -0500

    calendar

--- src/components/ui/DatePicker/StyledDatepicker.styled.jsx
+++ src/components/ui/DatePicker/StyledDatepicker.styled.jsx
@@ -185,3 +185,18 @@ export const TitleWrapper = styled.button`
   justify-content: space-around;
   align-items: center;
 `;
+
+// export const TitleWrapper = styled.button`
+//   width: 195px;
+//   color: #ffffff;
+//   background: #ef8964;
+//   border-radius: 8px;
+//   font-weight: 700;
+//   line-height: 18px;
+//   text-align: center;
+//   text-transform: uppercase;
+//   padding: 8px 12px;
+//   font-size: 16px;
+//   justify-content: space-around;
+//   align-items: center;
+// `;
