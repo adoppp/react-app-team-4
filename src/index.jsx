f30bda4543c09c4b7af0fab223be075f6ab10f92
viktoriia-2002 <92129770+viktoriia-2002@users.noreply.github.com>
2023-11-10 21:59:17 -0500

    Calendar

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,11 @@ import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
 import './index.scss';
 
-import DatePickerCalendar from '../src/components/ui/DatePicker/DatePicker';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/react-app-team-4">
       <App />
-      <DatePickerCalendar />
     </BrowserRouter>
   </React.StrictMode>,
 );
