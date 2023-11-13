e2f78524b4929294b1a08eed93e2c94b912501f4
vovaprodan <121820923+vovaprodan@users.noreply.github.com>
2023-11-13 17:03:36 +0100

    fixProductsItem

--- src/components/ProductsItem/ProductsItem.jsx
+++ src/components/ProductsItem/ProductsItem.jsx
@@ -11,36 +11,36 @@ const ProductsItem = () => {
     
   } 
      const IconStyles = {
-    backgroundColor:' #EFA082',
-    
+        backgroundColor: ' #EFA082',
+        width: 24,
+        height: 24,
+        borderRadius: 50,
+         
+   } 
+     const IconButtonStyles = {
+         marginLeft: 8,
+ 
   } 
     
     return <li className={cn('item')}>
         <div className={cn('item_container')}>
             <p className={cn('diet')} >DIET</p>
-            <p>Recommended</p>
-            <Button
-            type={'submit'}
-        customContainerStyles={ButtonStyles}
-            >
-            <span>
-            </span>
-          
-            </Button>
-            <button>
+            <div className={cn('button_container')}>
+                   <p className={cn('text')}>Recommended</p>
+            <button className={cn('button')}>
                 Add
-                <Icon iconId='icon-next' w={20}  h={20} customStyles={{ marginRight: 8 }} />
+                <Icon iconId='icon-next' w={20}  h={20} customStyles={IconButtonStyles} />
+               
             </button>
-              
-           
+         </div>  
         </div >
-        <div>
+        <div className={cn('title_container')} >
               <Icon
             iconId='icon-running-stick'
-            w={36}
-            h={13}
+            w={12}
+            h={14}
             customStyles={IconStyles}/>
-        <h2>Bread Hercules grain</h2>
+        <h2 className={cn('title')} >Bread Hercules grain</h2>
       </div>
         <div className={cn('item_list_container')}>
             <p>Calories:</p>
