83303e0c9f6536e697a26c9c638b07bd95c1eb0e
Vladimir Hanhur <aqgz@email.cz>
2023-11-13 10:22:41 +0100

    feature(ExercisesPage): task_description

--- src/components/pages/ExercisesPage/ExercisesPage.jsx
+++ src/components/pages/ExercisesPage/ExercisesPage.jsx
@@ -1,7 +1,31 @@
+import classNames from 'classnames/bind';
+import styles from './ExercisesPage.module.scss';
+import { Link } from 'react-router-dom';
+import { Icon } from '../../ui/Icon/Icon';
+
+import ExercisesList from './ExercisesList/ExercisesList';
+
+const cn = classNames.bind(styles);
+
 const ExercisesPage = () => {
     return (
-        <div>
+        <div className={cn('container')}>
+            <Link to="/exercises" />
             <h2>ExercisesPage</h2>
+            <div className={cn('container_icon')}>
+                <Icon iconId='icon-arrow'
+                    w={16}
+                    h={16}
+                />
+                <p>Back</p>
+            </div>
+            <h2 className={cn('container_title')}>Waist</h2>
+            <div className={cn('container_box')}>
+                <p>Body parts</p>
+                <p>Muscles</p>
+                <p>Equipment</p>
+            </div>
+            <ExercisesList />
         </div>
     );
 };
