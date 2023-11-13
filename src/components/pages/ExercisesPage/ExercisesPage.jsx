77020d06f11dd12084913c529dc577ea88bec74f
KRVitalik <karmelitavitalik@gmail.com>
2023-11-13 17:35:14 +0200

    feature(ExercisesPage): create ExercisesPage component.

--- src/components/pages/ExercisesPage/ExercisesPage.jsx
+++ src/components/pages/ExercisesPage/ExercisesPage.jsx
@@ -1,9 +1,20 @@
-const ExercisesPage = () => {
-    return (
-        <div>
-            <h2>ExercisesPage</h2>
-        </div>
-    );
-};
+// import ExercisesCategories from './ExercisesCategories/ExercisesCategories'
+import classNames from 'classnames/bind';
+import styles from './ExercisesPage.module.scss';
 
-export default ExercisesPage;
+const cn = classNames.bind(styles);
+
+const ExercisesPage = () => {
+  return (
+    <>
+    <section className={cn('section')}>
+    <div>
+        <h2>ExercisesPage</h2>
+    </div>
+      {/* <ExercisesCategories/> */}
+    </section>
+    </>
+  )
+}
+
+export default ExercisesPage
