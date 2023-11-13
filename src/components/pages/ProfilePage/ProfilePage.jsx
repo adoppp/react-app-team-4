ac2df62a0b001362ecddbad5b77c70fcd53615ea
Adop <chdn786od@gmail.com>
2023-11-13 23:42:27 +0100

    Update ProfilePage.jsx

--- src/components/pages/ProfilePage/ProfilePage.jsx
+++ src/components/pages/ProfilePage/ProfilePage.jsx
@@ -1,15 +1,7 @@
-import { Modal } from "../../ui/Modal/Modal";
-
 const ProfilePage = () => {
     return (
         <div>
             <h2>ProfilePage</h2>
-            <Modal>
-                <div>
-                    <h3>lorem</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, enim? Animi atque similique, dolores voluptatum quis perferendis labore inventore tenetur fuga quam suscipit ratione harum molestiae dolorum?</p>
-                </div>
-            </Modal>
         </div>
     );
 };
