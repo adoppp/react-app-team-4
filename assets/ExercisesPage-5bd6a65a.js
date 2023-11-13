8fbbfb1693878e530adec9b101faf5f2d2daab30
adoppp <121878757+adoppp@users.noreply.github.com>
2023-11-13 23:01:07 +0000

    Deploying to gh-pages from @ adoppp/react-app-team-4@1cdf03816793694f827976b0bef2ef72da5b03ab 🚀

--- assets/ExercisesPage-5bd6a65a.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-9c60f7f8.js";const r=()=>e.jsx("div",{children:e.jsx("h2",{children:"ExercisesPage"})});export{r as default};
---
+++ assets/ProductsPage-8bf1eb5b.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-41ce77c3.js";const t=()=>r.jsx("div",{children:r.jsx("h2",{children:"ProductsPage"})});export{t as default};
