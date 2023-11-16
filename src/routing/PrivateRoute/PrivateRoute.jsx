a5e3961be45f4d882c517700ab6685a1ab804f1c
anastasiialukianenko <121677643+anastasiialukianenko@users.noreply.github.com>
2023-11-16 16:55:19 +0100

    updated(Routing): add logic to refreshing and main route

--- src/routing/PrivateRoute/PrivateRoute.jsx
+++ src/routing/PrivateRoute/PrivateRoute.jsx
@@ -1,13 +1,36 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 import { routes } from '../routes';
 import { autheticatedSelector } from '../../storage/selectors/authSelectors';
+import { refreshUser } from '../../storage/operations/authThunk';
 
 const PrivateRoute = ({ children, redirectTo = routes.SIGNIN_ROUTE }) => {
+    const [isLoading, setIsLoading] = useState(true);
     const authentificated = useSelector(autheticatedSelector);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const checkAuthentication = async () => {
+            try {
+                await dispatch(refreshUser());
+                setIsLoading(false);
+            } catch (error) {
+                console.error(error);
+                setIsLoading(false);
+            }
+        };
+        checkAuthentication();
+    }, [dispatch]); 
+    if (isLoading) {
+        return null;
+    }
 
     return authentificated ? children : <Navigate to={redirectTo} replace />;
 }
 
 export { PrivateRoute };
+    
+
+
