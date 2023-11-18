5608f18551f5468493ddc47cd82d8b8fd9631590
Iluha3443 <tkacenkoila01@gmail.com>
2023-11-18 23:09:15 +0200

    styles UserForm&UserCard

--- src/components/pages/ProfilePage/ProfilePage.jsx
+++ src/components/pages/ProfilePage/ProfilePage.jsx
@@ -1,14 +1,24 @@
 import {UserCard} from '../../UserCard';
 import { UserForm } from '../../ui/UserForm';
+import styles from './ProfilePage.module.scss';
+import classNames from 'classnames/bind';
+import { Title } from '../../global/Title/Title';
+
+const cn = classNames.bind(styles);
 
 const ProfilePage = () => {
+
     return (
-        <div>
-            <h2>ProfilePage</h2>
-            <UserCard />
-            <UserForm/>
+        <div className={cn('Profile__Settings')}>
+           <div className={cn('Profile__SettingsContainer')}>
+            <Title title={'Profile Settings'}/>
+            <div className={cn('container')}>
+                <UserCard />
+                <div className={cn('decorative-line')}></div>
+                <UserForm />
+            </div>
         </div>
-        
+        </div>
     );
 };
 
