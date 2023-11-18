efb205ed3c98489e856de7420281f07d004f3f5f
Adop <chdn786od@gmail.com>
2023-11-19 00:22:04 +0100

    feature(Header): add avatar path

--- src/storage/selectors/authSelectors.js
+++ src/storage/selectors/authSelectors.js
@@ -1,3 +1,5 @@
 export const autheticatedSelector = (state) => state.user.authenticated;
 
 export const tokenSelector = (state) => state.user.token;
+
+export const avatarSelector = (state) => state.user.userData.avatarURL;
