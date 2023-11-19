a76257b6fb67f0dcbbd6fe2ea7ab05fb8ceb5252
Vladimir Hanhur <aqgz@email.cz>
2023-11-19 15:08:58 +0100

    update(Exercises): Exercises

---
+++ src/components/ExercisesList/ExercisesList.jsx
@@ -0,0 +1,394 @@
+import styles from './ExercisesList.module.scss';
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import { Icon } from '../../../ui/Icon/Icon';
+
+const cn = classNames.bind(styles);
+
+const ExercisesList = () => {
+    return (
+        <div>
+            <Link to="/exercises" />
+            <div className={cn('block')}>
+                <div className={cn('content')}>
+                    <div className={cn('content_start')}>
+                        <h2 className={cn('content_start-title')}>WORKOUT</h2>
+                        <p className={cn('content_start-text')}>Start</p>
+                        <Icon iconId='icon-statr-arrow'
+                            w={16}
+                            h={16}
+                        />
+                    </div>
+                    <div className={cn('content_figure')}>
+                        <Icon iconId='icon-figure'
+                            w={24}
+                            h={24}
+                        />
+                        <h3 className={cn('content_figure-title')}>Air bike</h3>
+                    </div>
+                    <div className={cn('content_item')}>
+                        <p>Burned calories:</p><span>312</span>
+                        <p>Body part:</p><span>Waist</span>
+                        <p>Target:</p>
+                        <span className={cn('content_item-span')}>Abc</span>
+                    </div>
+                </div>
+
+
+                <div className={cn('content')}>
+                    <div className={cn('content_start')}>
+                        <h2 className={cn('content_start-title')}>WORKOUT</h2>
+                        <p className={cn('content_start-text')}>Start</p>
+                        <Icon iconId='icon-statr-arrow'
+                            w={16}
+                            h={16}
+                        />
+                    </div>
+                    <div className={cn('content_figure')}>
+                        <Icon iconId='icon-figure'
+                            w={24}
+                            h={24}
+                        />
+                        <h3 className={cn('content_figure-title')}>3/4 sit-up</h3>
+                    </div>
+                    <div className={cn('content_item')}>
+                        <p>Burned calories:</p><span>220</span>
+                        <p>Body part:</p><span>Waist</span>
+                        <p>Target:</p>
+                        <span className={cn('content_item-span')}>Abc</span>
+                    </div>
+                </div>
+
+                <div className={cn('content')}>
+                    <div className={cn('content_start')}>
+                        <h2 className={cn('content_start-title')}>WORKOUT</h2>
+                        <p className={cn('content_start-text')}>Start</p>
+                        <Icon iconId='icon-statr-arrow'
+                            w={16}
+                            h={16}
+                        />
+                    </div>
+                    <div className={cn('content_figure')}>
+                        <Icon iconId='icon-figure'
+                            w={24}
+                            h={24}
+                        />
+                        <h3 className={cn('content_figure-title')}>45° side ben</h3>
+                    </div>
+                    <div className={cn('content_item')}>
+                        <p>Burned calories:</p><span>323</span>
+                        <p>Body part:</p><span>Waist</span>
+                        <p>Target:</p>
+                        <span className={cn('content_item-span')}>Abc</span>
+                    </div>
+                </div>
+
+                <div className={cn('content')}>
+                    <div className={cn('content_start')}>
+                        <h2 className={cn('content_start-title')}>WORKOUT</h2>
+                        <p className={cn('content_start-text')}>Start</p>
+                        <Icon iconId='icon-statr-arrow'
+                            w={16}
+                            h={16}
+                        />
+                    </div>
+                    <div className={cn('content_figure')}>
+                        <Icon iconId='icon-figure'
+                            w={24}
+                            h={24}
+                        />
+                        <h3 className={cn('content_figure-title')}>Barbell reverse preacher curl</h3>
+                    </div>
+                    <div className={cn('content_item')}>
+                        <p>Burned calories:</p><span>153</span>
+                        <p>Body part:</p><span>Waist</span>
+                        <p>Target:</p>
+                        <span className={cn('content_item-span')}>Biceps</span>
+                    </div>
+                </div>
+
+                <div className={cn('content')}>
+                    <div className={cn('content_start')}>
+                        <h2 className={cn('content_start-title')}>WORKOUT</h2>
+                        <p className={cn('content_start-text')}>Start</p>
+                        <Icon iconId='icon-statr-arrow'
+                            w={16}
+                            h={16}
+                        />
+                    </div>
+                    <div className={cn('content_figure')}>
+                        <Icon iconId='icon-figure'
+                            w={24}
+                            h={24}
+                        />
+                        <h3 className={cn('content_figure-title')}>Barbell rollerout</h3>
+                    </div>
+                    <div className={cn('content_item')}>
+                        <p>Burned calories:</p><span>87</span>
+                        <p>Body part:</p><span>Waist</span>
+                        <p>Target:</p>
+                        <span className={cn('content_item-span')}>Abc</span>
+                    </div>
+                </div>
+
+                <div className={cn('content')}>
+                    <div className={cn('content_start')}>
+                        <h2 className={cn('content_start-title')}>WORKOUT</h2>
+                        <p className={cn('content_start-text')}>Start</p>
+                        <Icon iconId='icon-statr-arrow'
+                            w={16}
+                            h={16}
+                        />
+                    </div>
+                    <div className={cn('content_figure')}>
+                        <Icon iconId='icon-figure'
+                            w={24}
+                            h={24}
+                        />
+                        <h3 className={cn('content_figure-title')}>Barbell side split squat v. 2</h3>
+                    </div>
+                    <div className={cn('content_item')}>
+                        <p>Burned calories:</p><span>60</span>
+                        <p>Body part:</p><span>Waist</span>
+                        <p>Target:</p>
+                        <span className={cn('content_item-span')}>Quads</span>
+                    </div>
+                </div>
+
+                <div className={cn('content')}>
+                    <div className={cn('content_start')}>
+                        <h2 className={cn('content_start-title')}>WORKOUT</h2>
+                        <p className={cn('content_start-text')}>Start</p>
+                        <Icon iconId='icon-statr-arrow'
+                            w={16}
+                            h={16}
+                        />
+                    </div>
+                    <div className={cn('content_figure')}>
+                        <Icon iconId='icon-figure'
+                            w={24}
+                            h={24}
+                        />
+                        <h3 className={cn('content_figure-title')}>Bottoms-up</h3>
+                    </div>
+                    <div className={cn('content_item')}>
+                        <p>Burned calories:</p><span>234</span>
+                        <p>Body part:</p><span>Waist</span>
+                        <p>Target:</p>
+                        <span className={cn('content_item-span')}>Abc</span>
+                    </div>
+                </div>
+
+                <div className={cn('content')}>
+                    <div className={cn('content_start')}>
+                        <h2 className={cn('content_start-title')}>WORKOUT</h2>
+                        <p className={cn('content_start-text')}>Start</p>
+                        <Icon iconId='icon-statr-arrow'
+                            w={16}
+                            h={16}
+                        />
+                    </div>
+                    <div className={cn('content_figure')}>
+                        <Icon iconId='icon-figure'
+                            w={24}
+                            h={24}
+                        />
+                        <h3 className={cn('content_figure-title')}>Cable lying fly</h3>
+                    </div>
+                    <div className={cn('content_item')}>
+                        <p>Burned calories:</p><span>61</span>
+                        <p>Body part:</p><span>Waist</span>
+                        <p>Target:</p>
+                        <span className={cn('content_item-span')}>Pectorals</span>
+                    </div>
+                </div>
+                <div className={cn('content')}>
+                    <div className={cn('content_start')}>
+                        <h2 className={cn('content_start-title')}>WORKOUT</h2>
+                        <p className={cn('content_start-text')}>Start</p>
+                        <Icon iconId='icon-statr-arrow'
+                            w={16}
+                            h={16}
+                        />
+                    </div>
+                    <div className={cn('content_figure')}>
+                        <Icon iconId='icon-figure'
+                            w={24}
+                            h={24}
+                        />
+                        <h3 className={cn('content_figure-title')}>Cable lying fly</h3>
+                    </div>
+                    <div className={cn('content_item')}>
+                        <p>Burned calories:</p><span>61</span>
+                        <p>Body part:</p><span>Waist</span>
+                        <p>Target:</p>
+                        <span className={cn('content_item-span')}>Pectorals</span>
+                    </div>
+                </div>
+                <div className={cn('content')}>
+                    <div className={cn('content_start')}>
+                        <h2 className={cn('content_start-title')}>WORKOUT</h2>
+                        <p className={cn('content_start-text')}>Start</p>
+                        <Icon iconId='icon-statr-arrow'
+                            w={16}
+                            h={16}
+                        />
+                    </div>
+                    <div className={cn('content_figure')}>
+                        <Icon iconId='icon-figure'
+                            w={24}
+                            h={24}
+                        />
+                        <h3 className={cn('content_figure-title')}>Cable lying fly</h3>
+                    </div>
+                    <div className={cn('content_item')}>
+                        <p>Burned calories:</p><span>61</span>
+                        <p>Body part:</p><span>Waist</span>
+                        <p>Target:</p>
+                        <span className={cn('content_item-span')}>Pectorals</span>
+                    </div>
+                </div>
+                <div className={cn('content')}>
+                    <div className={cn('content_start')}>
+                        <h2 className={cn('content_start-title')}>WORKOUT</h2>
+                        <p className={cn('content_start-text')}>Start</p>
+                        <Icon iconId='icon-statr-arrow'
+                            w={16}
+                            h={16}
+                        />
+                    </div>
+                    <div className={cn('content_figure')}>
+                        <Icon iconId='icon-figure'
+                            w={24}
+                            h={24}
+                        />
+                        <h3 className={cn('content_figure-title')}>Cable lying fly</h3>
+                    </div>
+                    <div className={cn('content_item')}>
+                        <p>Burned calories:</p><span>61</span>
+                        <p>Body part:</p><span>Waist</span>
+                        <p>Target:</p>
+                        <span className={cn('content_item-span')}>Pectorals</span>
+                    </div>
+                </div>
+                <div className={cn('content')}>
+                    <div className={cn('content_start')}>
+                        <h2 className={cn('content_start-title')}>WORKOUT</h2>
+                        <p className={cn('content_start-text')}>Start</p>
+                        <Icon iconId='icon-statr-arrow'
+                            w={16}
+                            h={16}
+                        />
+                    </div>
+                    <div className={cn('content_figure')}>
+                        <Icon iconId='icon-figure'
+                            w={24}
+                            h={24}
+                        />
+                        <h3 className={cn('content_figure-title')}>Cable lying fly</h3>
+                    </div>
+                    <div className={cn('content_item')}>
+                        <p>Burned calories:</p><span>61</span>
+                        <p>Body part:</p><span>Waist</span>
+                        <p>Target:</p>
+                        <span className={cn('content_item-span')}>Pectorals</span>
+                    </div>
+                </div>
+                <div className={cn('content')}>
+                    <div className={cn('content_start')}>
+                        <h2 className={cn('content_start-title')}>WORKOUT</h2>
+                        <p className={cn('content_start-text')}>Start</p>
+                        <Icon iconId='icon-statr-arrow'
+                            w={16}
+                            h={16}
+                        />
+                    </div>
+                    <div className={cn('content_figure')}>
+                        <Icon iconId='icon-figure'
+                            w={24}
+                            h={24}
+                        />
+                        <h3 className={cn('content_figure-title')}>Cable lying fly</h3>
+                    </div>
+                    <div className={cn('content_item')}>
+                        <p>Burned calories:</p><span>61</span>
+                        <p>Body part:</p><span>Waist</span>
+                        <p>Target:</p>
+                        <span className={cn('content_item-span')}>Pectorals</span>
+                    </div>
+                </div>
+                <div className={cn('content')}>
+                    <div className={cn('content_start')}>
+                        <h2 className={cn('content_start-title')}>WORKOUT</h2>
+                        <p className={cn('content_start-text')}>Start</p>
+                        <Icon iconId='icon-statr-arrow'
+                            w={16}
+                            h={16}
+                        />
+                    </div>
+                    <div className={cn('content_figure')}>
+                        <Icon iconId='icon-figure'
+                            w={24}
+                            h={24}
+                        />
+                        <h3 className={cn('content_figure-title')}>Cable lying fly</h3>
+                    </div>
+                    <div className={cn('content_item')}>
+                        <p>Burned calories:</p><span>61</span>
+                        <p>Body part:</p><span>Waist</span>
+                        <p>Target:</p>
+                        <span className={cn('content_item-span')}>Pectorals</span>
+                    </div>
+                </div>
+                <div className={cn('content')}>
+                    <div className={cn('content_start')}>
+                        <h2 className={cn('content_start-title')}>WORKOUT</h2>
+                        <p className={cn('content_start-text')}>Start</p>
+                        <Icon iconId='icon-statr-arrow'
+                            w={16}
+                            h={16}
+                        />
+                    </div>
+                    <div className={cn('content_figure')}>
+                        <Icon iconId='icon-figure'
+                            w={24}
+                            h={24}
+                        />
+                        <h3 className={cn('content_figure-title')}>Cable lying fly</h3>
+                    </div>
+                    <div className={cn('content_item')}>
+                        <p>Burned calories:</p><span>61</span>
+                        <p>Body part:</p><span>Waist</span>
+                        <p>Target:</p>
+                        <span className={cn('content_item-span')}>Pectorals</span>
+                    </div>
+                </div>
+                <div className={cn('content')}>
+                    <div className={cn('content_start')}>
+                        <h2 className={cn('content_start-title')}>WORKOUT</h2>
+                        <p className={cn('content_start-text')}>Start</p>
+                        <Icon iconId='icon-statr-arrow'
+                            w={16}
+                            h={16}
+                        />
+                    </div>
+                    <div className={cn('content_figure')}>
+                        <Icon iconId='icon-figure'
+                            w={24}
+                            h={24}
+                        />
+                        <h3 className={cn('content_figure-title')}>Cable lying fly</h3>
+                    </div>
+                    <div className={cn('content_item')}>
+                        <p>Burned calories:</p><span>61</span>
+                        <p>Body part:</p><span>Waist</span>
+                        <p>Target:</p>
+                        <span className={cn('content_item-span')}>Pectorals</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ExercisesList;
--- src/components/pages/ExercisesPage/ExercisesPage.jsx
+++ src/components/pages/ExercisesPage/ExercisesPage.jsx
@@ -7,6 +7,11 @@ import styles from './ExercisesPage.module.scss';
 import { Title } from '../../global/Title';
 import { ExercisesCategories } from '../../ExercisesCategories/ExercisesCategories';
 
+import ExercisesList from '../../ExercisesList/ExercisesList';
+
+import { Link } from 'react-router-dom';
+import { Icon } from '../../ui/Icon/Icon';
+
 const cn = classNames.bind(styles);
 
 const ExercisesPage = () => {
@@ -19,6 +24,30 @@ const ExercisesPage = () => {
       <Suspense fallback={<Loader/>}>
         <Outlet />
      </Suspense>
+     <div className={cn('container')}>
+            <div className={cn('container_item')}>
+                <Link to="/exercises" />
+                {/* <h2>ExercisesPage</h2> */}
+                <div className={cn('container_block')}>
+                    <div>
+                        <div className={cn('container_icon')}>
+                            <Icon iconId='icon-arrow'
+                                w={16}
+                                h={16}
+                            />
+                            <p>Back</p>
+                        </div>
+                        <h2 className={cn('container_title')}>Waist</h2>
+                    </div>
+                    <div className={cn('container_box')}>
+                        <p>Body parts</p>
+                        <p>Muscles</p>
+                        <p>Equipment</p>
+                    </div>
+                </div>
+                <ExercisesList />
+            </div>
+        </div>
     </section>
   );
 };
