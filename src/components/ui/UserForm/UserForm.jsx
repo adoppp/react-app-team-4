902bc09e0218f20f062fc909ed729386ab2174f4
Adop <chdn786od@gmail.com>
2023-11-20 11:26:49 +0100

    refactoring(ProfilePage): fix styles, validation.

--- src/components/ui/UserForm/UserForm.jsx
+++ src/components/ui/UserForm/UserForm.jsx
@@ -45,7 +45,7 @@ const UserForm = () => {
     birthday: '',
     blood: '',
     sex: '',
-    levelActivity: '',
+    levelActivity: 0,
   };
 
   const handleSubmit = (values) => {
@@ -59,7 +59,7 @@ const UserForm = () => {
         validationSchema={validationSchema}
         onSubmit={handleSubmit}
       >
-        {({ errors, touched, values }) => (
+        {({ errors, touched }) => (
           <Form>
             <div className={cn('basic__infoNameContainer')}>
               <div className={cn('basic__infoName')}>
@@ -122,25 +122,158 @@ const UserForm = () => {
             <div className={cn('basic__infoInputContainer')}>
               <div className={cn('basic__info')}>
                 <label>Height</label>
-                <Field type="number" name="height" component={Input} />
-                <ErrorMessage name="height" component="div" />
+                <Field type="number" name="height" className={cn(
+                  'input',
+                  { error: errors.height && touched.height },
+                  { success: !errors.height && touched.height },
+                )} />
+                {errors.height && touched.height ? (
+                    <div
+                        className={cn({
+                            errorLabel:
+                                errors.height && touched.height,
+                        })}
+                    >
+                        <Icon
+                            iconId="icon-checkbox-circle"
+                            w={16}
+                            h={16}
+                            customStyles={{
+                                marginRight: 4,
+                                fill: '#D80027',
+                            }}
+                        />
+                        <span>{errors.height}</span>
+                    </div>
+                ) : null}
+                {!errors.height && touched.height ? (
+                    <div
+                        className={cn({
+                            successLabel:
+                                !errors.height && touched.height,
+                        })}
+                    >
+                        <Icon
+                            iconId="icon-checkbox-circle"
+                            w={16}
+                            h={16}
+                            customStyles={{
+                                marginRight: 4,
+                                fill: '#3CBF61',
+                            }}
+                        />
+                        <span>{`Success height`}</span>
+                    </div>
+                ) : null
+                }
               </div>
 
               <div className={cn('basic__info')}>
                 <label>Current Weight</label>
-                <Field type="number" name="currentWeight" component={Input} />
-                <ErrorMessage name="currentWeight" component="div" />
+                <Field type="number" name="currentWeight" className={cn(
+                  'input',
+                  { error: errors.currentWeight && touched.currentWeight },
+                  { success: !errors.currentWeight && touched.currentWeight },
+                )} />
+                {errors.currentWeight && touched.currentWeight ? (
+                    <div
+                        className={cn({
+                            errorLabel:
+                                errors.currentWeight && touched.currentWeight,
+                        })}
+                    >
+                        <Icon
+                            iconId="icon-checkbox-circle"
+                            w={16}
+                            h={16}
+                            customStyles={{
+                                marginRight: 4,
+                                fill: '#D80027',
+                            }}
+                        />
+                        <span>{errors.currentWeight}</span>
+                    </div>
+                ) : null}
+                {!errors.currentWeight && touched.currentWeight ? (
+                    <div
+                        className={cn({
+                            successLabel:
+                                !errors.currentWeight && touched.currentWeight,
+                        })}
+                    >
+                        <Icon
+                            iconId="icon-checkbox-circle"
+                            w={16}
+                            h={16}
+                            customStyles={{
+                                marginRight: 4,
+                                fill: '#3CBF61',
+                            }}
+                        />
+                        <span>{`Success current weight`}</span>
+                    </div>
+                ) : null
+                }
               </div>
 
               <div className={cn('basic__info')}>
                 <label>Desired Weight</label>
-                <Field type="number" name="desiredWeight" component={Input} />
-                <ErrorMessage name="desiredWeight" component="div" />
+                <Field type="number" name="desiredWeight" className={cn(
+                  'input',
+                  { error: errors.desiredWeight && touched.desiredWeight },
+                  { success: !errors.desiredWeight && touched.desiredWeight },
+                )} />
+                {errors.desiredWeight && touched.desiredWeight ? (
+                    <div
+                        className={cn({
+                            errorLabel:
+                                errors.desiredWeight && touched.desiredWeight,
+                        })}
+                    >
+                        <Icon
+                            iconId="icon-checkbox-circle"
+                            w={16}
+                            h={16}
+                            customStyles={{
+                                marginRight: 4,
+                                fill: '#D80027',
+                            }}
+                        />
+                        <span>{errors.desiredWeight}</span>
+                    </div>
+                ) : null}
+                {!errors.desiredWeight && touched.desiredWeight ? (
+                    <div
+                        className={cn({
+                            successLabel:
+                                !errors.desiredWeight && touched.desiredWeight,
+                        })}
+                    >
+                        <Icon
+                            iconId="icon-checkbox-circle"
+                            w={16}
+                            h={16}
+                            customStyles={{
+                                marginRight: 4,
+                                fill: '#3CBF61',
+                            }}
+                        />
+                        <span>{`Success desired weight`}</span>
+                    </div>
+                ) : null
+                }
               </div>
 
               <div className={cn('basic__info')}>
                 <div className={cn('basic__calendary')}>
-                  <Field type="date" name="birthday" component={Input} />
+                  <Field type="date" name="birthday" className={cn(
+                    'input',
+                    // { error: errors.name && touched.name },
+                    // { success: !errors.name && touched.name },
+                    'bdInput'
+                  )}
+                    style={{ marginBottom: 0, marginTop: 22 }}
+                  />
                 </div>
             
               </div>
@@ -148,65 +281,195 @@ const UserForm = () => {
             <div className={cn('blood__containerParent')}>
               <div>
                 <label className={cn('label__blood')}>Blood</label>
-                <div className={cn('blood__container')} >
+                <div className={cn('blood__container')} style={{marginBottom: 8}}>
                   <label>
-                    <Field type="radio" id="blood1" name="blood" value="1" />
+                    <Field type="radio" id="blood1" name="blood" value="1" className={cn('radioBtn')} />
+                    <span className={cn('customRadioBtn')}></span>
                     <p className={cn('circle')}>1</p>
                   </label>
                   <label htmlFor="blood2">
-                    <Field type="radio" id="blood2" name="blood" value="2" />
+                    <Field type="radio" id="blood2" name="blood" value="2" className={cn('radioBtn')} />
+                    <span className={cn('customRadioBtn')}></span>
                     <p className={cn('circle')}>2</p>
                   </label>
                   <label htmlFor="blood3">
-                    <Field type="radio" id="blood3" name="blood" value="3" />
+                    <Field type="radio" id="blood3" name="blood" value="3" className={cn('radioBtn')} />
+                    <span className={cn('customRadioBtn')}></span>
                     <p className={cn('circle')}>3</p>
                   </label>
                   <label htmlFor="blood4">
-                    <Field type="radio" id="blood4" name="blood" value="4" />
+                    <Field type="radio" id="blood4" name="blood" value="4" className={cn('radioBtn')} />
+                    <span className={cn('customRadioBtn')}></span>
                     <p className={cn('circle')}>4</p>
                   </label>
                 </div>
-                <ErrorMessage name="blood" component="div" />
+                {errors.blood && touched.blood ? (
+                    <div
+                        className={cn({
+                            errorLabel:
+                                errors.blood && touched.blood,
+                        })}
+                    >
+                        <Icon
+                            iconId="icon-checkbox-circle"
+                            w={16}
+                            h={16}
+                            customStyles={{
+                                marginRight: 4,
+                                fill: '#D80027',
+                            }}
+                        />
+                        <span>{errors.blood}</span>
+                    </div>
+                ) : null}
+                {!errors.blood && touched.blood ? (
+                    <div
+                        className={cn({
+                            successLabel:
+                                !errors.blood && touched.blood,
+                        })}
+                    >
+                        <Icon
+                            iconId="icon-checkbox-circle"
+                            w={16}
+                            h={16}
+                            customStyles={{
+                                marginRight: 4,
+                                fill: '#3CBF61',
+                            }}
+                        />
+                        <span>{`Success blood`}</span>
+                    </div>
+                ) : null
+                }
               </div>
 
-              <div className={cn('sex__container')} >
-                <label className={cn('sex__label')}>
-                  <Field type="radio" name="sex" value="Male" />
-                  <p className={cn('circle')}>Male</p>
-                </label>
-                <label className={cn('sex__label')}>
-                  <Field type="radio" name="sex" value="Female" />
-                  <p className={cn('circle')}>Female</p>
-                </label>
-                <ErrorMessage name="sex" component="div" />
-              </div>
+            <div className={cn('sex__container')} >
+                <div className={cn('sex__wrapper')}>
+                    <div className={cn('sex__checkbox-wrapper')}>
+                        <label className={cn('sex__label')}>
+                        <Field type="radio" name="sex" value="male" className={cn('radioBtn')} />
+                        <span className={cn('customRadioBtn')}></span>
+                        <p className={cn('circle')}>Male</p>
+                        </label>
+                        <label className={cn('sex__label')}>
+                    <Field type="radio" name="sex" value="female" className={cn('radioBtn')} />
+                    <span className={cn('customRadioBtn')}></span>
+                    <p className={cn('circle')}>Female</p>
+                        </label>
+                    </div>
+                    {errors.sex && touched.sex ? (
+                        <div
+                            className={cn({
+                                errorLabel:
+                                    errors.sex && touched.sex,
+                            })}
+                        >
+                            <Icon
+                                iconId="icon-checkbox-circle"
+                                w={16}
+                                h={16}
+                                customStyles={{
+                                    marginRight: 4,
+                                    fill: '#D80027',
+                                }}
+                            />
+                            <span>{errors.sex}</span>
+                        </div>
+                    ) : null}
+                    {!errors.sex && touched.sex ? (
+                        <div
+                            className={cn({
+                                successLabel:
+                                    !errors.sex && touched.sex,
+                            })}
+                        >
+                            <Icon
+                                iconId="icon-checkbox-circle"
+                                w={16}
+                                h={16}
+                                customStyles={{
+                                    marginRight: 4,
+                                    fill: '#3CBF61',
+                                }}
+                            />
+                            <span>{`Success sex`}</span>
+                        </div>
+                    ) : null
+                    }
+                </div>
+                </div>
             </div>
           
             <div className={cn('levelActivity__container')}>
               <label className={cn('levelActivity__label')} htmlFor="levelActivity1">
-                <Field type="radio" id="levelActivity1" name="levelActivity" value="Sedentary" />
+                <Field type="radio" id="levelActivity1" name="levelActivity" value="1" className={cn('radioBtn')} />
+                <span className={cn('customRadioBtn')}></span>
                 <p className={cn('circle')}>Sedentary lifestyle (little or no physical activity)</p>
               </label>
               <label className={cn('levelActivity__label')} htmlFor="levelActivity2">
-                <Field type="radio" id="levelActivity2" name="levelActivity" value="Light" />
+                <Field type="radio" id="levelActivity2" name="levelActivity" value="2" className={cn('radioBtn')} />
+                <span className={cn('customRadioBtn')}></span>
                 <p className={cn('circle')}>Light activity (light exercises/sports 1-3 days per week)</p>
               </label>
               <label className={cn('levelActivity__label')} htmlFor="levelActivity3">
-                <Field type="radio" id="levelActivity3" name="levelActivity" value="Moderately" />
+                <Field type="radio" id="levelActivity3" name="levelActivity" value="3" className={cn('radioBtn')} />
+                <span className={cn('customRadioBtn')}></span>
                 <p className={cn('circle')}>Moderately active (moderate exercises/sports 3-5 days per week)</p>
               </label>
               <label className={cn('levelActivity__label')} htmlFor="levelActivity4">
-                <Field type="radio" id="levelActivity4" name="levelActivity" value="Very" />
+                <Field type="radio" id="levelActivity4" name="levelActivity" value="4" className={cn('radioBtn')} />
+                <span className={cn('customRadioBtn')}></span>
                 <p className={cn('circle')}>Very active (intense exercises/sports 6-7 days per week)</p>
               </label>
               <label className={cn('levelActivity__label')} htmlFor="levelActivity5">
-                <Field type="radio" id="levelActivity5" name="levelActivity" value="Extremely" />
+                <Field type="radio" id="levelActivity5" name="levelActivity" value="5" className={cn('radioBtn')} />
+                <span className={cn('customRadioBtn')}></span>
                 <p className={cn('circle')}>Extremely active (very strenuous exercises/sports and physical work)</p>
-              </label>
+                </label>
+                {errors.levelActivity && touched.levelActivity ? (
+                <div
+                    className={cn({
+                        errorLabel:
+                            errors.levelActivity && touched.levelActivity,
+                    })}
+                >
+                    <Icon
+                        iconId="icon-checkbox-circle"
+                        w={16}
+                        h={16}
+                        customStyles={{
+                            marginRight: 4,
+                            fill: '#D80027',
+                        }}
+                    />
+                    <span>{errors.levelActivity}</span>
+                </div>
+            ) : null}
+            {!errors.levelActivity && touched.levelActivity ? (
+                <div
+                    className={cn({
+                        successLabel:
+                            !errors.levelActivity && touched.levelActivity,
+                    })}
+                >
+                    <Icon
+                        iconId="icon-checkbox-circle"
+                        w={16}
+                        h={16}
+                        customStyles={{
+                            marginRight: 4,
+                            fill: '#3CBF61',
+                        }}
+                    />
+                    <span>{`Success level activity`}</span>
+                </div>
+            ) : null
+            }
             </div>
 
             <div>
-              <Button label="Save" />
+              <Button label="Save" type='submit' />
             </div>
           </Form>
         )}
