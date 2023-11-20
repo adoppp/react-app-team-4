9e25a568a0cecd03970fb0c31687356b6285d442
KRVitalik <karmelitavitalik@gmail.com>
2023-11-20 13:01:18 +0200

    feature(ExercisesPage): big commit

--- src/components/pages/ExercisesPage/ExercisesPage.jsx
+++ src/components/pages/ExercisesPage/ExercisesPage.jsx
@@ -1,19 +1,31 @@
 import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
-import { Loader } from '../../ui/Loader/Loader';
+import { Link, Outlet, useParams } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
 import styles from './ExercisesPage.module.scss';
 import { Title } from '../../global/Title';
 import { ExercisesCategories } from './ExercisesCategories';
+import { Icon } from '../../ui/Icon';
+import { Loader } from '../../ui/Loader/Loader';
 
 const cn = classNames.bind(styles);
 
 const ExercisesPage = () => {
+  const params = useParams();
+
+  const upperCaseName = (el) => el[0].toUpperCase() + el.slice(1)
+
+  const title = params.workout ? upperCaseName(params.workout) : 'Exercises'
+
+
   return (
     <section className={cn('exercises')}>
+          {params.workout && <Link className={cn('exercises__button_back')} to=".." relative="path" >
+        <Icon iconId="icon-arrow" w={16} h={16}/>
+        <p>Back</p>
+    </Link>}
       <div className={cn('exercises__categories')}>
-        <Title title='Exercises' />
+        <Title title={`${title}`} />
         <ExercisesCategories/>
       </div>
       <Suspense fallback={<Loader/>}>
---
+++ src/storage/operations/exercisesThunk.js
@@ -0,0 +1,28 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { instance } from './authThunk';
+
+const REJECTED = (thunkAPI, e) => thunkAPI.rejectWithValue(e.message);
+
+export const getAllExercises = createAsyncThunk(
+    'getAll/exercises',
+    async (data, thunkAPI) => {
+        try {
+            const response = await instance.get('exercises', data);
+            return response.data;
+        } catch (e) {
+            return REJECTED(thunkAPI, e);
+        }
+    },
+);
+
+export const exercisesCategory = createAsyncThunk(
+    'getAll/category',
+    async (data, thunkAPI) => {
+        try {
+            const response = await instance.get('exercises/filters', data);
+            return response.data;
+        } catch (e) {
+            return REJECTED(thunkAPI, e);
+        }
+    },
+);
