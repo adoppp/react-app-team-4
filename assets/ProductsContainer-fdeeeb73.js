a2b5586ff2aec302e6a0be1e016dd4e495bfbbec
adoppp <121878757+adoppp@users.noreply.github.com>
2023-11-25 04:34:47 +0000

    Deploying to gh-pages from @ adoppp/react-app-team-4@deeaee98c7215a98fa7ffdc26be94177ca553119 🚀

---
+++ assets/ProductsContainer-fdeeeb73.js
@@ -0,0 +1,1 @@
+import{c as a,j as s}from"./index-a23cab31.js";import{H as c}from"./Header-a0add15b.js";const r="_container_74l42_1",t="_background_74l42_23",e={container:r,background:t},n=a.bind(e),l=({children:o})=>s.jsxs("section",{className:n("background"),children:[s.jsx(c,{}),s.jsx("div",{className:n("container"),children:o})]});export{l as default};
