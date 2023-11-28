6b09088dc1c6f27bee990a19e91abf340f9bde89
adoppp <121878757+adoppp@users.noreply.github.com>
2023-11-28 19:22:02 +0000

    Deploying to gh-pages from @ adoppp/react-app-team-4@35da4f7cf3522e36c1e744d7bcf984266d1a005b 🚀

---
+++ assets/PrivateContainer-98e85220.js
@@ -0,0 +1,1 @@
+import{c as t,j as s}from"./index-88d5a374.js";import{H as e}from"./Header-c34a6686.js";const a="_container_1ws8g_1",o={container:a},r=t.bind(o),m=({children:n})=>s.jsxs("section",{children:[s.jsx(e,{}),s.jsx("div",{className:r("container"),children:n})]});export{m as default};
