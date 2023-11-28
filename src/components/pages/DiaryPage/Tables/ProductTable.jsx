1ff32b785f630fb905f1870b2ad50057435ca29f
KRVitalik <karmelitavitalik@gmail.com>
2023-11-28 21:37:56 +0200

    fix(Table): rename ExercisesTable.module.scss => Table.module.scss

--- src/components/pages/DiaryPage/Tables/ProductTable.jsx
+++ src/components/pages/DiaryPage/Tables/ProductTable.jsx
@@ -2,7 +2,7 @@ import classNames from 'classnames/bind';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import styles from './ExercisesTable.module.scss';
+import styles from './Table.module.scss';
 import { Icon } from '../../../ui/Icon';
 import {
     deleteProduct,
